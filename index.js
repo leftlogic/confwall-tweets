d5e8b353d8bacff224e877a01c4df00b078437e9
Remy Sharp <remy@remysharp.com>
2014-12-11 10:45:25 +0000

    mongodb support

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+var MongoClient = require('mongodb').MongoClient;
 var JSONStream = require('JSONStream');
 var fs = require('fs');
 var colors = require('colors');
@@ -39,14 +39,39 @@ var expandLinks = function (tweet) {
   return text;
 };
 
-if (!process.argv[2] || !fs.existsSync(process.argv[2])) {
-  console.log('Usage: ')
+
+var tweet = function (data) {
+  if (!data.retweeted_status) {
+    console.log((data.user.name + ' (@' + data.user.screen_name + ')').bold + ' ' + expandLinks(data) + '\n' + ('https://twitter.com/' + data.user.screen_name + '/status/' + data.id_str + ' @ ' + data.created_at).grey + '\n');
+  }
+};
+
+var target = process.argv[2] || '';
+
+if (target.indexOf('mongodb://') === 0 && process.argv[3]) {
+  // read from mongo
+  MongoClient.connect(target, function(error, db) {
+    if (error) {
+      throw error;
+    }
+
+    var collection = db.collection(process.argv[3]);
+    // Find some documents
+    var stream = collection.find().stream();
+
+    // Execute find on all the documents
+    stream.on('end', function() {
+      db.close();
+    });
+
+    stream.on('data', tweet);
+  });
+} else if (fs.existsSync(target)) {
+  fs.createReadStream(process.argv[2]).pipe(JSONStream.parse('*', tweet));
+}
+
+if (!target) {
+  console.log('Usage: confwall-tweets [filename.json | mongodb://<host> <collection>]')
   process.exit(1);
 }
 
-fs.createReadStream(process.argv[2])
-  .pipe(JSONStream.parse('*', function (data) {
-    if (!data.retweeted_status) {
-      console.log((data.user.name + ' (@' + data.user.screen_name + ')').bold + ' ' + expandLinks(data) + '\n' + ('https://twitter.com/' + data.user.screen_name + '/status/' + data.id_str + ' @ ' + data.created_at).grey + '\n');
-    }
-  }));
